861d31b72405fcafb3775dc5cc0e1c3a854d673b
rockwolf <thereisanewway@gmail.com>
2014-04-18 11:18:02 +0200

    Updated with the extra comands.
    Note, this is still untested.

--- freebsd/report_income_vs_expenses.sh
+++ freebsd/report_income_vs_expenses.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Compare income vs expenses per year
+# Compare income vs expenses
 
 Usage()
 {
@@ -23,21 +23,21 @@ case $# in
     ;;
 2) 
     # Plot all data for the given year.
-    ledger -f $1 --real -s -p $2 -d "T&l<=1" bal --yearly --period-sort -Equity -^assets expenses income
+    ledger -f $1 --real -s -p $2 -d "T&l<=1" bal --period-sort --yearly -Equity -^assets expenses income
     ;;
 3)
     # Plot all data for the given period.
+    ledger -f $1 --real -s -d "T&l<=1" -b $2 -e $3 bal --period-sort --monthly -Equity -^assets expenses income
     ;;
 4)
     if [ "$4" = "total" ];
     then
         # Plot all data for the given period
         ## as the total
-        
+        ledger -f $1 --real -s -d "T&l<=1" -b $2 -e $3 bal -Equity -^assets expenses income
     else
         Usage
     fi
-
     ;;
 *)
     Usage()
