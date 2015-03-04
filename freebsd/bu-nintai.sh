30556ee48b20c197e0564c20d884d7b622e9f667
rockwolf <thereisanewway@gmail.com>
2015-03-04 13:18:41 +0100

    Fancify.

--- freebsd/bu-nintai.sh
+++ freebsd/bu-nintai.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-#
+
+################################################################################
 # Author: Andy Nagels
 # Date: 2015-03-04
 # 
 # bu-nintai.sh:
 # Creates a backup of the prd dir
+################################################################################
 
 DIR=/home/rockwolf/nintai
 PREFIX=/mnt/data01/bak/nintai
