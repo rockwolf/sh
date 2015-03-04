220b30a2ddd50d507dbba437875ea2d5200e3d5d
rockwolf <thereisanewway@gmail.com>
2015-03-04 14:33:47 +0100

    Fancify

--- freebsd/startwm.sh
+++ freebsd/startwm.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+################################################################################
+# Author: Andy Nagels
+# Date: 2015-03-04
+#
+# startwm.sh:
+# Start window manager.
+################################################################################
+
 #xrandr --output --screen 0 --mode 1440x900
 #xrandr --output --screen 1 --right-of --mode 1440x900
 #DISPLAY=:0.1 exec dwm &
