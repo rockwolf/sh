110bde12c7b1ef52969c1126e2b2b7bfa0e78204
rockwolf <thereisanewway@gmail.com>
2015-03-04 14:35:09 +0100

    Fancify.

--- freebsd/mousemover.fish
+++ freebsd/mousemover.fish
@@ -1,5 +1,6 @@
 #!/usr/local/bin/fish
-#
+
+################################################################################
 # Author: Andy Nagels
 # Date: 2015-03-04
 #
@@ -7,6 +8,7 @@
 # This is a small script that triggers a keyboard event
 # to keep the screen from blanking when watching movies on the web.
 # Ctr+C to quit.
+################################################################################
 
 set INTERVAL 300 # interval in seconds
 
