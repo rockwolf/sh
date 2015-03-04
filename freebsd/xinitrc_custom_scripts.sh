dc76e99b47697a43dd91ee9e1991b4e751814f94
rockwolf <thereisanewway@gmail.com>
2015-03-04 13:28:50 +0100

    Created for use in xinitrc

---
+++ freebsd/xinitrc_custom_scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+################################################################################
+# Author: Andy Nagels
+# Date: 2015-03-04
+#
+# xinitrc_custom_scripts.sh:
+# Execute custom scripts and applications in xinitrc.
+################################################################################
+
+#urxvtd &
+wallpaper.sh
+timetokyo.sh
