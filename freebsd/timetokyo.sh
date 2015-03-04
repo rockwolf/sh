2aaa4cdadea82159e13d5d409f13d4162e3dc5da
rockwolf <thereisanewway@gmail.com>
2015-03-04 14:21:37 +0100

    Fancify.

--- freebsd/timetokyo.sh
+++ freebsd/timetokyo.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+################################################################################
+# Author: Andy Nagels
+# Date: 2015-03-04
+#
+# timetokyo.sh:
+# Script that shows clocks in different timezones.
+################################################################################
+
 DATEFORMAT="%Y-%m-%d"
 TIMEFORMAT="%H:%M"
 
