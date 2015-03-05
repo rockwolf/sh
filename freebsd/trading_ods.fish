f4b1fe0ca55edd5c25e06c9e42c5d35f7e2f956b
rockwolf <thereisanewway@gmail.com>
2015-03-05 18:32:44 +0100

    Fancified the sh script and made a fish variant.

--- freebsd/trading_ods.fish
+++ freebsd/trading_ods.fish
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/usr/local/bin/fish
 
 ################################################################################
 # Author: Andy Nagels
 # Date: 2015-03-05
 #
-# trading_ods.sh:
+# trading_ods.fish:
 # This script cd's to the prd dir for nintai
 # and loads the given ods with soffice.
 ################################################################################
 
-DIR=$HOME/nintai/prd/doc
-ODS=trade_20140311_V001_position_trading.ods
+set DIR $HOME/nintai/prd/doc
+set ODS trade_20140311_V001_position_trading.ods
 
 cd $DIR
 soffice $ODS&
