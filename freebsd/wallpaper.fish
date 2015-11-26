5e58fc5eecb266aaabca1a20e26ad98e35c8ab3e
rockwolf <thereisanewway@gmail.com>
2015-11-26 11:35:20 +0100

    Better header.

--- freebsd/wallpaper.fish
+++ freebsd/wallpaper.fish
@@ -1,10 +1,12 @@
 #!/usr/local/bin/fish
-#
+
+################################################################################
 # Author: Andy Nagels
 # Date: 2015-03-04
 #
 # wallpaper.fish
-# Creates a backup of the doc and arc dir.
+# Sets the wallpaper.
+################################################################################
 
 set WALLDIR /home/rockwolf/wall
 
