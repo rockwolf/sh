de429fc6baa91d8c23a211bff687a532be183880
rockwolf <thereisanewway@gmail.com>
2015-11-26 10:22:48 +0100

    Converted to fish code.

--- freebsd/bu-nintai.fish
+++ freebsd/bu-nintai.fish
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin/fish
 
 ################################################################################
 # Author: Andy Nagels
@@ -9,87 +9,72 @@
 ################################################################################
 
 # TODO: rewrite this syntax to fish syntax
-DIR=/home/rockwolf/nintai
-PREFIX=/mnt/data01/bak/nintai
-USAGE="Usage: bu-nintai"
-OK="backup [OK]"
-DIR1=/home/rockwolf/nintai/prd
-NAME1=prd
-DIR2=/home/rockwolf/nintai/tst
-NAME2=tst
+set DIR /home/rockwolf/nintai
+set PREFIX /mnt/data01/bak/nintai
+set USAGE "Usage: bu-nintai"
+set OK "backup [OK]"
+set DIR1 /home/rockwolf/nintai/prd
+set NAME1 prd
+set DIR2 /home/rockwolf/nintai/tst
+test NAME2 tst
 
-fBackupDaily()
-{
-    OLDDIR=$PREFIX/daily/old
-    NEWDIR=$PREFIX/daily/new
-    
+function fBackupDaily
+    set OLDDIR $PREFIX/daily/old
+    set NEWDIR $PREFIX/daily/new
     fClearBackup $OLDDIR $NEWDIR
     echo $NAME1" - daily "$OK
     echo $NAME2" - daily "$OK
-    
     ### make backup
-    tar czf $NEWDIR/$NAME1`date +%Y-%m-%d`.tar.gz $DIR1
-    tar czf $NEWDIR/$NAME2`date +%Y-%m-%d`.tar.gz $DIR2
-}
+    tar czf $NEWDIR/$NAME1(date +%Y-%m-%d).tar.gz $DIR1
+    tar czf $NEWDIR/$NAME2(date +%Y-%m-%d).tar.gz $DIR2
+end
 
-fBackupWeekly()
-{
-    OLDDIR=$PREFIX/weekly/old
-    NEWDIR=$PREFIX/weekly/new
-    
+function fBackupWeekly
+    set OLDDIR $PREFIX/weekly/old
+    set NEWDIR $PREFIX/weekly/new
     fClearBackup $OLDDIR $NEWDIR
     echo $NAME1" - weekly "$OK
     echo $NAME2" - weekly "$OK
-    
     ### make backup
-    tar czf $NEWDIR/$NAME1`date +%Y-%m-%d`.tar.gz $DIR1
-    tar czf $NEWDIR/$NAME2`date +%Y-%m-%d`.tar.gz $DIR2
-}
+    tar czf $NEWDIR/$NAME1(date +%Y-%m-%d).tar.gz $DIR1
+    tar czf $NEWDIR/$NAME2(date +%Y-%m-%d).tar.gz $DIR2
+end
 
-fBackupMonthly()
-{
-    OLDDIR=$PREFIX/monthly/old
-    NEWDIR=$PREFIX/monthly/new
-    
+function fBackupMonthly
+    set OLDDIR $PREFIX/monthly/old
+    set NEWDIR $PREFIX/monthly/new
     fClearBackup $OLDDIR $NEWDIR
     echo $NAME1" - monthly "$OK
     echo $NAME2" - monthly "$OK
-    
     ### make backup
-    tar czf $NEWDIR/$NAME1`date +%Y-%m-%d`.tar.gz $DIR1
-    tar czf $NEWDIR/$NAME2`date +%Y-%m-%d`.tar.gz $DIR2
-}
+    tar czf $NEWDIR/$NAME1(date +%Y-%m-%d).tar.gz $DIR1
+    tar czf $NEWDIR/$NAME2(date +%Y-%m-%d).tar.gz $DIR2
+end
 
-fClearBackup()
-{
+function fClearBackup
     ### remove previous bak-ups
     rm $OLDDIR/*.tar.gz
-
     ### move current backup from new to old
     mv $NEWDIR/*.tar.gz $OLDDIR
-}
+end
 
-fUsage()
-{
+function fUsage
     echo $USAGE
     exit 1
-}
+end
 
-case $# in
-0)
-    fBackupDaily
-    DOW=`date +%u`
-    DOM=`date +%d`
-    if [ "$DOW" = "1" ];
-    then
-        fBackupWeekly
-    fi
-    if [ "$DOM" = "01" ];
-    then
-        fBackupMonthly
-    fi
-    ;;
-*)
-    fUsage
-    ;;
-esac
+set args (fish -c "for el in $args; echo \$el; end")
+switch (count $args)
+    case 0
+        fBackupDaily
+        set DOW (date +%u)
+        set DOM (date +%d)
+    if math "$DOW" == "1" > /dev/null
+            fBackupWeekly
+        end
+        if math "$DOM" == "1" > /dev/null
+          fBackupMonthly
+        end
+    case '*'
+        fUsage
+end
