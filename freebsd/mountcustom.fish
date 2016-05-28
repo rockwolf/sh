dbc410418a4c583199ad61b3a17ac7776936f110
rockwolf <thereisanewway@gmail.com>
2016-05-28 22:21:03 +0200

    Mounting of data01 added. Improved the script in general.

--- freebsd/mountcustom.fish
+++ freebsd/mountcustom.fish
@@ -12,6 +12,24 @@ set DATEFORMAT "%Y-%m-%d"
 set TIMEFORMAT "%H:%M"
 set KEYLOCATION "/bootdir/boot/"
 
-# tar00:
-geli attach -k "$KEYLOCATION/encryption_tar00.key" "/dev/da0p1"
-zpool import tar00
+switch $argv[1]
+case "-h"
+    echo "mountcustom.fish [-h|tar00|data01]"
+    echo "Note: Needs to be run as root."
+case "tar00"
+    echo Mounting tar00...
+    geli attach -k "$KEYLOCATION/encryption_tar00.key" "/dev/da0p1"
+    zpool import tar00
+    echo Done. 
+    true
+case "data01"
+    echo Mounting data01...
+    geli attach /dev/ada2
+    geli attach /dev/ada3
+    zpool import data01
+    echo Done. 
+    true
+case '*'
+    echo Unknown option $argv[1].
+    false
+end
