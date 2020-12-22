8fcc299cced9d8cf1b4a714657c653c443a3b129
Andy Nagels <thereisanewway@gmail.com>
2020-12-22 18:16:15 +0100

    Changes to the xinitrc scripts.

--- freebsd/xinitrc_settings_language.fish
+++ freebsd/xinitrc_settings_language.fish
@@ -12,17 +12,17 @@
 #set -x LANG=es_ES.ISO8859-1 LLC_ALL=es_ES.ISO8859-1
 #set -x LANG=es_ES.UTF-8 LLC_ALL=es_ES.UTF-8
 
-set -x LANG "ja_JP.UTF-8"
-set -x LLC_ALL "ja_JP.UTF-8"
-set -x XIM "ibus"
-set -x GTK_IM_MODULE "ibus"
-set -x QT_IM_MODULE "xim"
-set -x XMODIFIERS "@im=ibus"
-set -x XIM_PROGRAM "ibus-daemon"
-set -x XIM_ARGS "-r --daemonize --xim"
-
-# Start uim-xim for Japanese language input.
-# 2015-07-30: switched to ibus
-# See: http://redundancy.redundancy.org/fbsd_japanese.html
-#uim-xim
-ibus-daemon -r --daemonize --xim
+#set -x LANG "ja_JP.UTF-8"
+#set -x LLC_ALL "ja_JP.UTF-8"
+#set -x XIM "ibus"
+#set -x GTK_IM_MODULE "ibus"
+#set -x QT_IM_MODULE "xim"
+#set -x XMODIFIERS "@im=ibus"
+#set -x XIM_PROGRAM "ibus-daemon"
+#set -x XIM_ARGS "-r --daemonize --xim"
+#
+## Start uim-xim for Japanese language input.
+## 2015-07-30: switched to ibus
+## See: http://redundancy.redundancy.org/fbsd_japanese.html
+##uim-xim
+#ibus-daemon -r --daemonize --xim
