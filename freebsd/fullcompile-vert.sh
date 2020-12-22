87aa0803d2df6642cedc2bca1c6648900f8801b4
Andy Nagels <thereisanewway@gmail.com>
2020-12-22 17:59:45 +0100

    Latex compilation script for vertical Japanese text.

---
+++ freebsd/fullcompile-vert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+################################################################################
+# Author: Andy Nagels
+# Date: 2015-03-04
+#
+# fullcompile.sh:
+# Compiles a latex document, twice.
+# Note: the parameter should be the name of the document, without the extension!
+################################################################################
+
+uplatex $1.tex
+dvipdfmx $1
+#dvips -Ppdf -G0 $1.dvi -o $1.ps 
+##G0: bug in Ghostscript ontwijken
+#ps2pdf -dPDFsettings=/prepress $1.ps $1.pdf
+##dPDFsettings=/.. => eps geen kwaliteitsverlies bij omzetting naar pdf
